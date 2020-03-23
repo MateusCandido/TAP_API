c92a4906f228d42eca5651e9b1cd3ba778b75d3c
MateusCandido <mateuscandidodebarros@gmail.com>
2020-03-23 12:52:13 -0400

    tap-api02

---
+++ CursoController.js
@@ -0,0 +1,7 @@
+Curso = {        
+    "titulo"​ :​ "JavaScript"​ ,    ​
+    "professor"​ :​ "Virmerson"​ ,    ​ 
+    "cargaHoraria"​ :​ "60 horas"​ ,    ​ 
+    "dataInicio"​ :​ "18/03/2020"​ ,    ​ 
+    "dataTermino"​ :​ "18/03/2020" 
+} 
