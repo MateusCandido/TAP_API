afdcfb4becf086d82beef7ae3d9b5132bd123476
MateusCandido <36897763+MateusCandido@users.noreply.github.com>
2020-04-22 18:46:25 -0400

    Add files via upload

--- TAP_API/CursoController.js
+++ TAP_API/CursoController.js
@@ -1,23 +1,24 @@
+const cursoModel = require('./models/CourseModel.js')
+
 class CursoController{
     static async listarCursoJson (req,res){
-        var curso = {
-            "titulo" : "JavaScript" ,
-            "professor" : "Virmerson" ,
-            "cargaHoraria" : "60 horas" ,
-            "dataInicio" : "18/03/2020" ,
-            "dataTermino" : "18/03/2020"
-        }
-        res.json(curso);
+        // var curso = {
+        //     "titulo" : "JavaScript" ,
+        //     "professor" : "Virmerson" ,
+        //     "cargaHoraria" : "60 horas" ,
+        //     "dataInicio" : "18/03/2020" ,
+        //     "dataTermino" : "18/03/2020"
+        // }
+        res.json(cursoModel);
     }
-
-    static async listaCurso(req, res) {
-        let lista = await CourseModel.find({})
-        res.json('lista')
+    static async listarCurso(req, res) {
+        let lista = await cursoModel.find({})
+        res.json(lista)
+        
     }
-
-    static cadastraCurso(req, res) {
-        CourseModel.create(req.body)
-        res.send('Cadastrando realizado com sucesso')
+    static cadastrarCurso(req, res) {
+        cursoModel.create(req.body)
+        res.send('Cadastro realizado com sucesso')
     }
 }
 module.exports = CursoController
--- TAP_API/server.js
+++ TAP_API/server.js
@@ -20,6 +20,7 @@ const port = process.env.PORT || 3000
 app.listen(port, () => { console.log('Server Ligado') });
 
 //MongoDB
+
 const mongoose = require('mongoose');
 mongoose.connect(
 'mongodb+srv://mateus:preto.preto@cluster0-uypf5.mongodb.net/test?retryWrites=true&w=majority', {
@@ -32,3 +33,4 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     console.log("Mongo Ligado")
 });
+
